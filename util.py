fdd70f6ef3840b7012e63424be7d91f31f37b395
madsies <madsiesss@gmail.com>
2025-08-13 22:06:08 +0100

    Resource Shuffled

--- util.py
+++ util.py
@@ -1,5 +1,5 @@
 import json
 from pathlib import Path
 
-with open(Path(__file__).parent / "data.json", "r", encoding="utf-8") as f:
+with open(Path(__file__).parent / "resources/data.json", "r", encoding="utf-8") as f:
     CONFIG_DATA = json.load(f)
