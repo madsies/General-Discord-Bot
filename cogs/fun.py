ce108a8a0a79f6fd88fbc81033649f31ccdf1ada
madsies <madsiesss@gmail.com>
2025-08-15 02:00:21 +0100

    Completed poke command implementation

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,8 +1,14 @@
-from random import Random
+import random
 import discord
 from typing import Literal, Optional
 from discord.ext import commands
-import util
+
+"""
+
+    Fun Commands
+    - Poke: Good example of grouped commands used in hybrid
+
+"""
 
 
 class FunCommands(commands.Cog):
@@ -14,17 +20,24 @@ class FunCommands(commands.Cog):
         await ctx.send("Please specify 'user' or 'random'.")
 
     @poke.command(name="user")
-    async def poke_user(self, ctx, user: discord.Member, silent: Literal[True, False] = False):
-        if (silent):  await ctx.send(f"Poking {user.nick}!")
-        else: await ctx.send(f"Poking {user.mention}!")
+    async def poke_user(self, ctx, user : discord.Member, silent: Literal["True", "False"] = "True"):
+        print(user)
+        sender : discord.Member = ctx.author
+        if (user == sender): 
+            await ctx.send(f"{sender.mention} tried to poke themselves...")
+            await sender.send(f"Why are you poking yourself...")
+        if (silent == "True"):  await ctx.send(f"{sender.mention} Poked {user.display_name}!")
+        else: await ctx.send(f"{sender.mention} Poked {user.mention}!")
 
     @poke.command(name="random")
-    async def poke_random(self, ctx, silent: Literal[True, False] = False):
-        user : discord.Member = ctx.guild.members[Random.randint(0, len(ctx.guild.members) - 1)]
-        if (silent):  await ctx.send(f"Poking {user.nick}!")
-        else: await ctx.send(f"Poking {user.mention}!")
+    async def poke_random(self, ctx, silent: Literal["True", "False"] = "True"):
+        sender : discord.Member = ctx.author
+        members = [member for member in ctx.guild.members if member != sender]
+        user : discord.Member = random.choice(members)
+        
+        if (silent == "True"):  await ctx.send(f"{sender.mention} Poked {user.display_name}!")
+        else: await ctx.send(f"{sender.mention} Poked {user.mention}!")
             
-
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(FunCommands(bot))
